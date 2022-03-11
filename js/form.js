56540876ec89ffd5d603a6f1a5457f55de6c2b24
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2022-03-11 08:06:17 +0100

    form kész

--- js/form.js
+++ js/form.js
@@ -1,16 +1,28 @@
-age = document.getElementById('age').value;
+// -- Popup --
+
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("sub");
 var span = document.getElementsByClassName("close")[0];
-// alert(age);
+
 
 btn.onclick = function() {
     modal.style.display = "block";
+    age = document.getElementById("age").value;
+    lname = document.getElementById("lname").value;
+    email = document.getElementById("email").value;
+    // alert(age);
+    if(age < 12) {
+      document.getElementById("modal-content-1").innerHTML = "Sajnáljuk, de csak <b>12 év felett</b> jelentkezhetsz a tanfolyamunkra.";
+    } else {
+      document.getElementById("modal-content-1").innerHTML = "Köszönjük a jelentkezésedet, " + "<i>" +lname + "</i>" + "! A részletekről értesítünk erre az E-mail címre: " + "<b>" + email + "</b>."; 
+    }
+    
   }
   
 
 span.onclick = function() {
     modal.style.display = "none";
+    document.location.reload(true)
 }
 
 window.onclick = function(event) {
@@ -18,3 +30,7 @@ window.onclick = function(event) {
       modal.style.display = "block";
     }
 }
+
+// -- Popup Content --
+
+
