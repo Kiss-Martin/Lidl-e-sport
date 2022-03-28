902401854c2a83581d98e8d830f86a43db342eb9
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2022-03-28 10:06:24 +0200

    form.js bugfixd

--- js/form.js
+++ js/form.js
@@ -8,14 +8,27 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
     age = document.getElementById("age").value;
+    fname = document.getElementById("fname").value;
     lname = document.getElementById("lname").value;
     email = document.getElementById("email").value;
+    game = document.getElementById("game").value;
+    country = document.getElementById("country").value;
+
+
     // alert(age);
-    if(age < 12) {
-      document.getElementById("modal-content-1").innerHTML = "Sajnáljuk, de csak <b>12 év felett</b> jelentkezhetsz a tanfolyamunkra.";
-    } else {
-      document.getElementById("modal-content-1").innerHTML = "Köszönjük a jelentkezésedet, " + "<i>" +lname + "</i>" + "! A részletekről értesítünk erre az E-mail címre: " + "<b>" + email + "</b>."; 
+
+    if(fname == "" || lname == "" || email == "" || game == "" || country == "") {
+      document.getElementById("modal-content-1").innerHTML = "<b>Nem töltötte ki az egész űrlapot!</b>";
     }
+    else {
+      if(age < 12) {
+        document.getElementById("modal-content-1").innerHTML = "Sajnáljuk, de csak <b>12 év felett</b> jelentkezhetsz a tanfolyamunkra.";
+      } else {
+        document.getElementById("modal-content-1").innerHTML = "Köszönjük a jelentkezésedet, " + "<i>" +lname + "</i>" + "! A részletekről értesítünk erre az E-mail címre: " + "<b>" + email + "</b>."; 
+      }
+    }
+
+    
     
   }
   
